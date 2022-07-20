2bce47594ca613302109e8e445a1caffadd8102e
theboringschool <theboringschool.org@gmail.com>
2022-07-21 00:05:48 +0545

    Wallet Work Done

--- Client/client/src/pages/99.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Nine9() {
-  return (
-    <div>Nine9</div>
-  )
-}
--- Client/client/src/pages/Trio.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Trio() {
-  return (
-    <div>Trio</div>
-  )
-}
